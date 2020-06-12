d0401cc5567b4ef505384c4b8a51289c00174176
Sergey <univ.silence@gmail.com>
2020-06-12 14:14:56 +0300

    Установил и подключил библиотеку prop-types

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {WelcomeScreen} from '@components/welcome-screen/welcome-screen.jsx';
 
 
 export const App = (props) => {
-  // eslint-disable-next-line react/prop-types
   const {errorCount} = props;
 
   return <WelcomeScreen
     errorCount = {errorCount}
   />;
+};
+
+App.propTypes = {
+  errorCount: PropTypes.number.isRequired,
 };
