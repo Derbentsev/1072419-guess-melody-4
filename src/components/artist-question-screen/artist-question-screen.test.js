11a231b1f959eeaaf85e228eff5d434f6d9941b1
Сергей Дербенцев <modcom@rambler.ru>
2020-06-25 16:31:44 +0300

    Обновил компонент audio-player

--- src/components/artist-question-screen/artist-question-screen.test.js
+++ src/components/artist-question-screen/artist-question-screen.test.js
@@ -24,7 +24,11 @@ it(`ArtistQuestionScreen is rendered correctly`, () => {
       <ArtistQuestionScreen
         question = {question}
         onAnswer = {() => {}}
-      />
+      />, {
+        createNodeMock: () => {
+          return {};
+        }
+      }
   ).toJSON();
 
   expect(tree).toMatchSnapshot();
