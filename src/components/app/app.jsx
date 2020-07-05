8af6302f0a93b38e22ce0533b74c6652ea3b4859
Sergey <univ.silence@gmail.com>
2020-07-05 18:30:36 +0300

    Описал логику полбеды и поражения в app. Доработал тесты

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,7 +3,9 @@ import ArtistQuestionScreen from '@components/artist-question-screen/artist-ques
 import GenreQuestionScreen from '@components/genre-question-screen/genre-question-screen';
 import GameScreen from '@components/game-screen/game-screen';
 import withActivePlayer from '@hocs/with-active-player/with-active-player';
-import withUserAnswer from '@hocs/with-user-answer/with-user-answer'
+import withUserAnswer from '@hocs/with-user-answer/with-user-answer';
+import GameOverScreen from '@components/game-over-screen/game-over-screen';
+import WinScreen from '@components/win-screen/win-screen';
 import {GameType} from '@consts/index';
 import {connect} from 'react-redux';
 import {ActionCreator} from '@reducer/reducer';
@@ -16,6 +18,7 @@ class App extends React.PureComponent {
   _renderGameScreen() {
     const {
       maxMistakes,
+      mistakes,
       questions,
       onUserAnswer,
       onWelcomeButtonClick,
@@ -23,11 +26,29 @@ class App extends React.PureComponent {
     } = this.props;
     const question = questions[step];
 
-    if (step === -1 || step >= questions.length) {
+    if (step === -1) {
       return (
         <WelcomeScreen
           errorsCount = {maxMistakes}
           onWelcomeButtonClick = {onWelcomeButtonClick}
+        />
+      );
+    }
+
+    if (mistakes >= maxMistakes) {
+      return (
+        <GameOverScreen
+          onReplayButtonClick = {() => {}}
+        />
+      );
+    }
+
+    if (step >= questions.length) {
+      return (
+        <WinScreen
+          questionsCount={questions.length}
+          mistakesCount={mistakes}
+          onReplayButtonClick={() => {}}
         />
       );
     }
@@ -91,6 +112,7 @@ class App extends React.PureComponent {
 
 App.propTypes = {
   maxMistakes: PropTypes.number.isRequired,
+  mistakes: PropTypes.number.isRequired,
   questions: PropTypes.array.isRequired,
   onUserAnswer: PropTypes.func.isRequired,
   onWelcomeButtonClick: PropTypes.func.isRequired,
@@ -101,6 +123,7 @@ const mapStateProps = (state) => ({
   step: state.step,
   maxMistakes: state.maxMistakes,
   questions: state.questions,
+  mistakes: state.mistakes,
 });
 
 const mapDispatchToProps = (dispatch) => ({
