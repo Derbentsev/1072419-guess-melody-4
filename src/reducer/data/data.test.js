bc84c0fa433791492e146995487a86c3d8a27b96
Sergey <univ.silence@gmail.com>
2020-07-12 12:14:17 +0300

    Написал асинхронные actions

--- src/reducer/data/data.test.js
+++ src/reducer/data/data.test.js
@@ -1,5 +1,9 @@
-import {reducer, ActionType} from './data';
+import {reducer, ActionType, Operation} from './data';
+import MockAdapter from 'axios-mock-adapter';
+import {createApi} from '@reducer/api';
 
+
+const api = createApi(() => {});
 
 const questions = [
   {
@@ -51,5 +55,26 @@ it(`Reducer should update questions by load questions`, () => {
     payload: questions,
   })).toEqual({
     questions,
+  });
+});
+
+describe(`Operation work correctly`, () => {
+  it(`Should make a correct API call to /questions`, function () {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const questionLoader = Operation.loadQuestions();
+
+    apiMock
+      .onGet(`/questions`)
+      .reply(200, [{fake: true}]);
+
+    return questionLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWidth(1, {
+          type: ActionType.LOAD_QUESTIONS,
+          payload: [{fake: true}],
+        });
+      });
   });
 });
